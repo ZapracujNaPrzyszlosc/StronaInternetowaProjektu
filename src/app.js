1ea3622ce654096482c9820a3fb867f7be0be5ba
philornot <philornot@users.noreply.github.com>
2025-03-03 17:40:33 +0100

    Pierwsza próba integracji z GA

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,10 +7,30 @@ import About from './pages/About';
 import TikTok from './pages/TikTok';
 import Contact from './pages/Contact';
 
+// Komponent do śledzenia zmiany stron
+function PageTracker() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    // Wysyłanie informacji o zmianie strony do dataLayer (GTM)
+    if (window.dataLayer) {
+      window.dataLayer.push({
+        event: 'page_view',
+        page: {
+          path: location.pathname + location.search
+        }
+      });
+    }
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
       <div className="app">
+        <PageTracker />
         <Header />
         <main className="main-content">
           <Routes>
